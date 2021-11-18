5bc33a927832ac084995e2f2ff339a9a8ddb5435
unknown <hoangcat622001@gmail.com>
2021-11-18 13:04:13 +0700

    separate client - server

--- server/server.js
+++ server/server.js
@@ -26,7 +26,7 @@ app.use("/api", orderRoutes);
 
 // connecting to mongoDB and then running server on port 4000
 const dbURI = config.get("dbURI");
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 4000;
 mongoose
     .connect(dbURI, {
         useNewUrlParser: true,
