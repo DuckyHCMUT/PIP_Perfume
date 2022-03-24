bc28ab53f98ca8444dabc0f5f46ae4fa3237e79b
Tran Quoc Viet <tqviet157@gmail.com>
2022-03-24 15:34:44 +0700

    Arranged front-end development view

--- server/routes/order.js
+++
@@ -1,8 +0,0 @@
-const { Router } = require("express");
-const orderController = require("../controllers/orderControllers.js");
-const router = Router();
-
-router.get("/order/:id", orderController.get_orders);
-//router.post("/order/:id", orderController.checkout);
-
-module.exports = router;
